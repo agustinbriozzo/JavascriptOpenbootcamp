cdada3cf8412fe664630581729aeab8c5c919997
Agustin Briozzo <agustinbriozzo1997@gmail.com>
2023-04-01 21:48:12 -0300

    ejercicio5

---
+++ ejercicio5.js
@@ -0,0 +1,7 @@
+let alturacm = 185
+let alturam = 1.85
+let pesokg = 88.5
+let alturaArriba = Math.ceil(alturam)
+let pesoAbajo = Math.floor(pesokg)
+let sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
