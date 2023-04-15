b2632aabd4b899946e89a5ff56fa31b50f67e192
Agustin Briozzo <agustinbriozzo1997@gmail.com>
2023-04-14 22:20:29 -0300

    gestion de errores

---
+++ 9.Errores/index.js
@@ -0,0 +1,26 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'error',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        //new winston.transports.File({ filename: 'combined.log' }),
+    ],
+});
+
+
+function retornaError() {
+    throw new Error("retornaError function");
+}
+
+try{
+    retornaError();
+} catch (e) {
+    logger.error("error" + e.toString());
+}
